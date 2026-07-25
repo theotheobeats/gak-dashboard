4340bce104930c7b9bfa84d617da70573c90eab4
Valentinus Theo <asakvsa.idn@gmail.com>
2026-07-25 18:57:45 +0700

    fix: linter fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,13 +1,16 @@
 import { PrismaClient } from "@prisma/client/edge";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
+const createPrismaClient = () =>
+  new PrismaClient().$extends(withAccelerate());
+
+type PrismaClientWithAccelerate = ReturnType<typeof createPrismaClient>;
+
 const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof withAccelerate> | undefined;
+  prisma: PrismaClientWithAccelerate | undefined;
 };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient({
-  accelerateUrl: process.env.DATABASE_URL,
-}).$extends(withAccelerate());
+export const prisma = globalForPrisma.prisma ?? createPrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
